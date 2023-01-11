bfa5bd8640bdfde3bf9a3363b6040f9cd754cf6e
asadi18 <asadishteaque@gmail.com>
2023-01-11 20:02:36 +0600

    Random background color genarete using javascript all options are available to select rgba and hsla

--- index.js
+++ index.js
@@ -1,92 +1,28 @@
-
 const button = document.getElementById('btn');
 const rgbaEl = document.getElementById('rgba');
+const changeColor = document.getElementById('changeColor')
+let color = 'rgba'
 
+changeColor.addEventListener("change", (e) => {
+  color = e.target.value
+  console.log(color)
+})
 
-
-button.addEventListener('click', function() {
+button.addEventListener('click', () => {
   
   const red = Math.floor(Math.random() * 256);
   const green = Math.floor(Math.random() * 256);
   const blue = Math.floor(Math.random() * 256);
-  const alpha = Math.random(); 
-
+  const alpha = Math.random().toFixed(2); 
   
   const hue = Math.floor(Math.random() * 361); 
   const saturation = Math.floor(Math.random() * 101); 
   const lightness = Math.floor(Math.random() * 101); 
 
-  //Work with this line 
-  const format = Math.random() < 0 ? 'hsl':'rgba' ;
+  const rgba = `rgba(${red}, ${green}, ${blue}, ${alpha})`;
+  const hsla = `hsla(${hue}, ${saturation}%, ${lightness}%, ${alpha})`;
 
-  let randomColor;
-    
-
-  if (format === 'rgba') {
-    
-    randomColor = `rgba(${red}, ${green}, ${blue}, ${alpha})`;
-  } else {
-    
-    randomColor = `hsla(${hue}, ${saturation}%, ${lightness}%, ${alpha})`;
-  }
-
-  
+  const randomColor = color === 'rgba' ? rgba : hsla;
   document.body.style.backgroundColor = randomColor;
-
-  
   document.getElementById('color-code').textContent = 'The Color code is : ' + randomColor;
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const button = document.getElementById('btn');
-
-
-// button.addEventListener('click', function() {
- 
-//   const red = Math.floor(Math.random() * 256);
-//   const green = Math.floor(Math.random() * 256);
-//   const blue = Math.floor(Math.random() * 256);
-//   const alpha = Math.random(); 
-
-
-//   const randomColor = `rgba(${red}, ${green}, ${blue}, ${alpha})`;
-
-  
-//   document.body.style.backgroundColor = randomColor;
-
-// });
-
-
-
-
-
-
